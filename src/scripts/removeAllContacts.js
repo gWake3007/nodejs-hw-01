45266a2d96285929962fdcf431d7404442341d5a
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-25 16:06:35 +0200

    1

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,12 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeAllContacts();
